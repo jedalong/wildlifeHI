7be4e713e84919545d7eaac28c50a1ee80e47d68
Jed Long <jed.a.long@gmail.com>
2023-05-18 15:23:00 -0400

    Update hi_get_osm.R

--- R/hi_get_osm.R
+++ R/hi_get_osm.R
@@ -26,7 +26,7 @@
 #' crossings.
 #'
 #' @return
-#'  This function returns an \code{sf} object containing OSM data.
+#'  This function returns an \code{sf} object containing OSM data. If the OSM query times out, a note is printed on the screen and the function returns \code{NULL}.
 #'
 #' @examples
 #' data(fishers)
@@ -48,55 +48,84 @@ hi_get_osm <- function(move,key='highway',value,bbox,geom="line",poly2line=TRUE)
     bbox <- bbox + c(-x10,-y10,x10,y10)
   }
   
-  if (missing(value)){
-    osmdata <- bbox |>
-      opq() |>
-      add_osm_feature(key = key) |>
-      osmdata_sf()
+  ## Error handling for large BBOX
+  ## ===============================================
+  
+  if (missing(value)) {
+    osmdata <- try ({
+      #could modify OSM values to facilitate larger bboxes (e.g., memsize or timeout)
+      opq (bbox = bbox) |>
+        add_osm_feature (key = key) |>
+        osmdata_sf ()
+    })
   } else {
-    osmdata <- bbox |>
-      opq() |>
-      add_osm_feature(key = key,value=value) |>
-      osmdata_sf()
+    osmdata <- try ({
+      #could modify OSM values to facilitate larger bboxes (e.g., memsize or timeout)
+      opq (bbox = bbox) |>
+        add_osm_feature (key = key, value=value) |>
+        osmdata_sf ()
+    })
   }
+
+  
+  if (class (osmdata) [1] == "try-error") {
+    writeLines('wildlifeHI Issue: OSM data request timed out. Suggestions include: \n
+               - Using the key and value parameters to choose less OSM features \n
+               - Subset tracking data to smaller areas \n
+               - Subset tracking data by individual(s) \n
+               Please see the wildlifeHI documentation for further discussion of this issue.')
+    return(NULL)
+  } 
+  
+
   
   osm_sf <- NULL
   if ('point' %in% geom) {
     temp_pt <- osmdata$osm_points
-    temp_pt$key <- key
-    temp_pt$value <- st_drop_geometry(temp_pt)[,key]
-    osm_sf <- rbind(osm_sf,temp_pt[,c('osm_id','name','key','value')])
+    if (length(temp_pt)>0) {
+      temp_pt$key <- key
+      temp_pt$value <- st_drop_geometry(temp_pt)[,key]
+      osm_sf <- rbind(osm_sf,temp_pt[,c('osm_id','name','key','value')])
+    }
   } 
   if ('line' %in% geom) {
     temp_ln <- osmdata$osm_lines
-    temp_ln$key <- key
-    temp_ln$value <- st_drop_geometry(temp_ln)[,key]
-    temp_ln <- temp_ln[,c('osm_id','name','key','value')]
-    if (!is.null(osmdata$osm_multilines)){
-      suppressWarnings(temp_ln2 <- osmdata$osm_multilines |> st_cast('LINESTRING'))
-      temp_ln2$key <- key
-      temp_ln2$value <- st_drop_geometry(temp_ln2)[,key]
-      temp_ln <- rbind(temp_ln,temp_ln2[,c('osm_id','name','key','value')])
+    if (length(temp_ln)>0) {
+      temp_ln$key <- key
+      temp_ln$value <- st_drop_geometry(temp_ln)[,key]
+      temp_ln <- temp_ln[,c('osm_id','name','key','value')]
+      if (!is.null(osmdata$osm_multilines)){
+        suppressWarnings(temp_ln2 <- osmdata$osm_multilines |> st_cast('LINESTRING'))
+        temp_ln2$key <- key
+        temp_ln2$value <- st_drop_geometry(temp_ln2)[,key]
+        temp_ln <- rbind(temp_ln,temp_ln2[,c('osm_id','name','key','value')])
+      }
+      osm_sf <- rbind(osm_sf,temp_ln)
     }
-    osm_sf <- rbind(osm_sf,temp_ln)
   }
   if ('polygon' %in% geom) {
     temp_po <- osmdata$osm_polygons
-    temp_po$key <- key
-    temp_po$value <- st_drop_geometry(temp_po)[,key]
-    temp_po <- temp_po[,c('osm_id','name','key','value')]
-    if (!is.null(osmdata$osm_multipolygons)){
-      suppressWarnings(temp_po2 <- osmdata$osm_multipolygons |> st_cast('POLYGON'))
-      temp_po2$key <- key
-      temp_po2$value <- st_drop_geometry(temp_po2)[,key]
-      temp_po <- rbind(temp_po,temp_po2[,c('osm_id','name','key','value')])
+    if (length(temp_po)>0) {
+      temp_po$key <- key
+      temp_po$value <- st_drop_geometry(temp_po)[,key]
+      temp_po <- temp_po[,c('osm_id','name','key','value')]
+      if (!is.null(osmdata$osm_multipolygons)){
+        suppressWarnings(temp_po2 <- osmdata$osm_multipolygons |> st_cast('POLYGON'))
+        temp_po2$key <- key
+        temp_po2$value <- st_drop_geometry(temp_po2)[,key]
+        temp_po <- rbind(temp_po,temp_po2[,c('osm_id','name','key','value')])
+      }
+      if (poly2line) {
+        suppressWarnings(temp_po <- st_cast(temp_po,"LINESTRING"))
+      }
+      osm_sf<- rbind(osm_sf, temp_po)
     }
-    if (poly2line) {
-      suppressWarnings(temp_po <- st_cast(temp_po,"LINESTRING"))
-    }
-    osm_sf<- rbind(osm_sf, temp_po)
   }
-  
+  if (is.null(osm_sf)){
+    writeLines('wildlifeHI Issue: The requested OSM query returned NULL. Please check:
+                  - The study area contains OSM features
+                  - The specified key and value inputs are correct')
+  }
   return(osm_sf)
   
 }
