4c3e36d5ab24ada146e497f309b7915680dcaf64
MattK-16 <116367795+MattK-16@users.noreply.github.com>
2024-03-26 20:11:11 -0400

    Resolving check() notes
    Created dummy internal R function for python function call (hi_rec_py), created aaa.R script to set internal state.

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,12 @@
+# ---- roxygen documentation ----
+#' @title for managing internal state
+#'
+#' @description
+#'   For managing internal state
+#'   
+#' @noRd
+# ---- End of roxygen documentation ----
+
 
 #managing environments in hi_recreation to minimize load times in subsequent runs
 the <- new.env(parent = emptyenv())
